298c13f274249ed2bcd593d8158e8d84649276ed
Monoget <monoget2@gmail.com>
2023-06-27 17:17:23 +0600

    update all

--- beautifulSoup.py
+++ beautifulSoup.py
@@ -1,23 +1,35 @@
-from bs4 import BeautifulSoup
-from selenium.common import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-import pandas as pd
-import csv
-import requests
 import time
+from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome()
 driver.maximize_window()
 
 
+driver.get("https://bet.hkjc.com/racing/pages/odds_wpq.aspx?lang=en&raceno=1")
+time.sleep(5)
+
+rows = driver.find_element(By.XPATH, '//*[@id="container"]/div/div/div[2]/div[2]').get_attribute("outerHTML")
+
+# Create a BeautifulSoup object and specify the parser
+soup = BeautifulSoup(rows, 'html.parser')
+
+# Find the parent <div> element
+parent_div = soup.find('div', class_='racebg')
+
+# Find all <div> child elements inside the parent <div>
+child_divs = parent_div.find_all('div')
+
+# Count the total number of <div> child elements
+total_child_divs = len(child_divs)
+
 j=1
 
-while j<8:
+while j<total_child_divs-1:
 
     driver.get("https://bet.hkjc.com/racing/pages/odds_wpq.aspx?lang=en&raceno="+str(j))
-    time.sleep(5)
+    time.sleep(2)
 
     rows = driver.find_elements(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr')
 
@@ -25,12 +37,7 @@ while j<8:
     for data in rows:
         i = i + 1
 
-    print(i)
-
-    fields = ['SL', "Horsename", 'Win', 'Place']
-    with open(r'Output/Race'+str(j)+'.csv', 'a', encoding="utf-8") as f:
-        writer = csv.writer(f)
-        writer.writerow(fields)
+    print('Race '+str(j)+' : rows '+str(i))
 
     x = 1
 
@@ -40,11 +47,9 @@ while j<8:
         win = driver.find_element(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr[' + str(x) + ']/td[5]').text
         place = driver.find_element(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr[' + str(x) + ']/td[6]').text
 
+        print('Win: '+win+' Place: '+place)
         x=x+1
 
-        fields = [sl, horsename, win, place]
-        with open(r'Output/Race'+str(j)+'.csv', 'a', encoding="utf-8") as f:
-            writer = csv.writer(f)
-            writer.writerow(fields)
-
     j=j+1
+
+    print('\n\n')
