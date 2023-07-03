d2caf280966c134787b0f774e5dcedf6ef46421a
Monoget <monoget2@gmail.com>
2023-07-03 11:37:41 +0600

    update all

--- main.py
+++ main.py
@@ -7,7 +7,18 @@ import requests
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-driver.get("https://bet.hkjc.com/racing/pages/odds_wpq.aspx?lang=ch&raceno=1")
+collect_url="https://bet.hkjc.com/racing/pages/odds_wpq.aspx?lang=ch&raceno="
+
+insert_url="http://localhost/W-Python-Data/"
+
+headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Other-Header": "Value"
+        }
+
+session = requests.Session()
+
+driver.get(collect_url+"1")
 time.sleep(5)
 
 rows = driver.find_element(By.XPATH, '//*[@id="container"]/div/div/div[2]/div[2]').get_attribute("outerHTML")
@@ -26,9 +37,9 @@ total_child_divs = len(child_divs)
 
 j=1
 
-while j<total_child_divs-3:
+while j<total_child_divs-1:
 
-    driver.get("https://bet.hkjc.com/racing/pages/odds_wpq.aspx?lang=ch&raceno="+str(j))
+    driver.get(collect_url+str(j))
     time.sleep(2)
 
     rows = driver.find_elements(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr')
@@ -42,13 +53,7 @@ while j<total_child_divs-3:
     x = 1
 
     if i==1:
-        url = "http://localhost/W-Python-Data/no_rows.php?page_no=" + str(j) +  "&rows=" + str(i)
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "Other-Header": "Value"
-        }
-
-        session = requests.Session()
+        url = insert_url+"no_rows.php?page_no=" + str(j) + "&rows=" + str(i)
 
         try:
             response = session.get(url, headers=headers)
@@ -66,27 +71,12 @@ while j<total_child_divs-3:
         win = driver.find_element(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr[' + str(x) + ']/td[5]').text
         place = driver.find_element(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr[' + str(x) + ']/td[6]').text
 
-        url = "http://localhost/W-Python-Data/receive_data.php?page_no="+str(j)+"&sl="+str(sl)+"&horse_name="+str(horsename)+"&win="+str(win)+"&place="+str(place) +"&rows="+str(i)  # Replace with your desired URL
-
-        # Set headers with a User-Agent and any additional required headers
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "Other-Header": "Value"
-        }
-
-        # Set up a session to manage cookies
-        session = requests.Session()
-
-        # Perform any necessary login or authentication steps
-        # ...
+        url = insert_url+"receive_data.php?page_no="+str(j)+"&sl="+str(sl)+"&horse_name="+str(horsename)+"&win="+str(win)+"&place="+str(place) +"&rows="+str(i)  # Replace with your desired URL
 
         try:
-            # Send the GET request with headers and cookies
             response = session.get(url, headers=headers)
 
-            # Check the response status code
             if response.status_code == 200:
-                # Successful response
                 print(response.text+" "+sl)
             else:
                 print(f"Request failed with status code: {response.status_code}")
@@ -98,3 +88,19 @@ while j<total_child_divs-3:
         x=x+1
 
     j=j+1
+
+
+url = insert_url+"no_page.php?page_no="+str(j)
+
+
+try:
+    response = session.get(url, headers=headers)
+
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+
+except requests.exceptions.RequestException as e:
+    # Error occurred during the request
+    print("An error occurred:", e)
