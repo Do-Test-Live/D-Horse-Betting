6ed77bee2cf6a8c3dc0ee6ecb17fd4c34eba11d1
Monoget <monoget2@gmail.com>
2023-07-03 11:41:29 +0600

    update all

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ import requests
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-collect_url="https://bet.hkjc.com/racing/pages/odds_wpq.aspx?lang=ch&raceno="
+collect_url = "https://bet.hkjc.com/racing/pages/odds_wpq.aspx?lang=ch&raceno="
 
-insert_url="http://localhost/W-Python-Data/"
+insert_url = "http://localhost/W-Python-Data/"
 
 headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-            "Other-Header": "Value"
-        }
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/91.0.4472.124 Safari/537.36",
+    "Other-Header": "Value"
+}
 
 session = requests.Session()
 
-driver.get(collect_url+"1")
+driver.get(collect_url + "1")
 time.sleep(5)
 
 rows = driver.find_element(By.XPATH, '//*[@id="container"]/div/div/div[2]/div[2]').get_attribute("outerHTML")
@@ -35,25 +36,25 @@ child_divs = parent_div.find_all('div')
 # Count the total number of <div> child elements
 total_child_divs = len(child_divs)
 
-j=1
+j = 1
 
-while j<total_child_divs-1:
+while j < total_child_divs - 1:
 
-    driver.get(collect_url+str(j))
+    driver.get(collect_url + str(j))
     time.sleep(2)
 
-    rows = driver.find_elements(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr')
+    rows = driver.find_elements(By.XPATH, '//*[@id="wpt' + str(j) + '"]/table/tbody/tr')
 
     i = 1
     for data in rows:
         i = i + 1
 
-    print('Race '+str(j)+' : rows '+str(i-1))
+    print('Race ' + str(j) + ' : rows ' + str(i - 1))
 
     x = 1
 
-    if i==1:
-        url = insert_url+"no_rows.php?page_no=" + str(j) + "&rows=" + str(i)
+    if i == 1:
+        url = insert_url + "no_rows.php?page_no=" + str(j) + "&rows=" + str(i)
 
         try:
             response = session.get(url, headers=headers)
@@ -65,19 +66,22 @@ while j<total_child_divs-1:
         except requests.exceptions.RequestException as e:
             print("An error occurred:", e)
 
-    while x<i-1:
-        sl = driver.find_element(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr[' + str(x) + ']/td[1]').text
-        horsename = driver.find_element(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr[' + str(x) + ']/td[4]').text
-        win = driver.find_element(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr[' + str(x) + ']/td[5]').text
-        place = driver.find_element(By.XPATH, '//*[@id="wpt'+str(j)+'"]/table/tbody/tr[' + str(x) + ']/td[6]').text
+    while x < i - 1:
+        sl = driver.find_element(By.XPATH, '//*[@id="wpt' + str(j) + '"]/table/tbody/tr[' + str(x) + ']/td[1]').text
+        horsename = driver.find_element(By.XPATH,
+                                        '//*[@id="wpt' + str(j) + '"]/table/tbody/tr[' + str(x) + ']/td[4]').text
+        win = driver.find_element(By.XPATH, '//*[@id="wpt' + str(j) + '"]/table/tbody/tr[' + str(x) + ']/td[5]').text
+        place = driver.find_element(By.XPATH, '//*[@id="wpt' + str(j) + '"]/table/tbody/tr[' + str(x) + ']/td[6]').text
 
-        url = insert_url+"receive_data.php?page_no="+str(j)+"&sl="+str(sl)+"&horse_name="+str(horsename)+"&win="+str(win)+"&place="+str(place) +"&rows="+str(i)  # Replace with your desired URL
+        url = insert_url + "receive_data.php?page_no=" + str(j) + "&sl=" + str(sl) + "&horse_name=" + str(
+            horsename) + "&win=" + str(win) + "&place=" + str(place) + "&rows=" + str(
+            i)  # Replace with your desired URL
 
         try:
             response = session.get(url, headers=headers)
 
             if response.status_code == 200:
-                print(response.text+" "+sl)
+                print(response.text + " " + sl)
             else:
                 print(f"Request failed with status code: {response.status_code}")
 
@@ -85,13 +89,11 @@ while j<total_child_divs-1:
             # Error occurred during the request
             print("An error occurred:", e)
 
-        x=x+1
+        x = x + 1
 
-    j=j+1
+    j = j + 1
 
-
-url = insert_url+"no_page.php?page_no="+str(j)
-
+url = insert_url + "no_page.php?page_no=" + str(j)
 
 try:
     response = session.get(url, headers=headers)
